31dc27f5b979ed9ff2da3a6846bd95ab4fa49c00
王杰 <1054918167@qq.com>
2018-09-25 17:36:26 +0800

    5.最长回文子串

---
+++ Arithmetic5.java
@@ -0,0 +1,83 @@
+package arithmetic;
+
+/**
+ * 题目:**最长回文子串
+ * 描述:
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为1000。
+ * 示例 1：
+ * 输入: "babad"
+ * 输出: "bab"
+ * 注意: "aba"也是一个有效答案。
+ * 示例 2：
+ * 输入: "cbbd"
+ * 输出: "bb"
+ * Created by WangJie on 2018/9/21.
+ */
+public class Arithmetic5 {
+
+    public static void main(String[] args) {
+        String s1 = "cbbd";
+        String s2 = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        System.out.println(longestPalindrome(s1));
+        System.out.println(longestPalindrome(s2));
+
+    }
+
+    public static String longestPalindrome(String s) {
+        if (s == null || "".equals(s)) {
+            return s;
+        }
+        String result = String.valueOf(s.charAt(0));
+        int l = result.length();
+        for (int i = 0; i < s.length() && l < s.length() - i; i++) {
+            int j = s.lastIndexOf(s.charAt(i));
+            while (j > 0 && j != i && (j + 1 - i) > l) {
+                String subStr = s.substring(i, j + 1);
+                //判断该字符串是否是回文子串，并且比现有的长
+                if (judge(subStr)) {
+                    result = subStr;
+                    l = result.length();
+                }
+                j = s.lastIndexOf(s.charAt(i), j - 1);
+            }
+        }
+        return result;
+    }
+
+    public static boolean judge(String str) {
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) == str.charAt(str.length() - 1 - i)) {
+                continue;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //========================================官方解决方案==========================================
+
+    public String longestPalindrome1(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+}
