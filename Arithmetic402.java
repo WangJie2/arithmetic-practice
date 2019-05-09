1bef2c26d227628f77f6759693fe8f93da39b937
Jie.Wang12 <Jie.Wang12@geely.com>
2019-05-09 09:43:48 +0800

    402.移掉K位数字

---
+++ Arithmetic402.java
@@ -0,0 +1,68 @@
+package arithmetic;
+
+/**
+ * 题目:**移掉K位数字
+ * 描述:
+ * 给定一个以字符串表示的非负整数 num，移除这个数中的 k 位数字，使得剩下的数字最小。
+ * 注意:
+ * num 的长度小于 10002 且 ≥ k。
+ * num 不会包含任何前导零。
+ * 示例 1 :
+ * 输入: num = "1432219", k = 3
+ * 输出: "1219"
+ * 解释: 移除掉三个数字 4, 3, 和 2 形成一个新的最小的数字 1219。
+ * 示例 2 :
+ * 输入: num = "10200", k = 1
+ * 输出: "200"
+ * 解释: 移掉首位的 1 剩下的数字为 200. 注意输出不能有任何前导零。
+ * 示例 3 :
+ * 输入: num = "10", k = 2
+ * 输出: "0"
+ * 解释: 从原数字移除所有的数字，剩余为空就是0。
+ * Created by WangJie on 2019/5/8.
+ */
+public class Arithmetic402 {
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        System.out.println(removeKdigits("1432219", 3));
+        System.out.println(removeKdigits("10200", 1));
+        System.out.println(removeKdigits("1234567890", 9));
+    }
+
+    /**
+     * 自己的方法
+     *
+     * @param num
+     * @param k
+     * @return
+     */
+    public static String removeKdigits(String num, int k) {
+        //新整数的最终长度 = 原整数长度 - k
+        int newLength = num.length() - k;
+        //创建一个栈，用于接收所有的数字
+        char[] stack = new char[num.length()];
+        int top = 0;
+        for (int i = 0; i < num.length(); ++i) {
+            //遍历当前数字
+            char c = num.charAt(i);
+            //当栈顶数字大于遍历到的当前数字，栈顶数字出栈（相当于删除数字）
+            while (top > 0 && stack[top - 1] > c && k > 0) {
+                top -= 1;
+                k -= 1;
+            }
+            //遍历到的当前数字入栈
+            stack[top++] = c;
+        }
+        // 找到栈中第一个非零数字的位置，以此构建新的整数字符串
+        int offset = 0;
+        while (offset < newLength && stack[offset] == '0') {
+            offset++;
+        }
+        return offset == newLength ? "0" : new String(stack, offset, newLength - offset);
+    }
+
+    //========================================官方解决方案==========================================
+
+
+}
